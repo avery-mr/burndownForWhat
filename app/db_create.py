c6d9ae2606b06d560cfadc0274639320bfbf5465
Bockslunch <155188366+Bockslunch@users.noreply.github.com>
2025-04-13 11:35:59 -0600

    Used db_utils to import get_connection() and execute_query()

--- app/db_create.py
+++ app/db_create.py
@@ -1,24 +1,7 @@
 import psycopg2
 import os
+from db_utils import get_connection, execute_query
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
-
-def execute_query(sql: str, message: str):
-    try:
-        conn = get_connection()
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit()
-        return message
-    except Exception as e:
-        print(f"Error: {e}")
-        return f"Error: {e}"
-    finally:
-        cur.close()
-        conn.close()
 
 def createUser():
     sql = ''' DROP TABLE IF EXISTS "User";
